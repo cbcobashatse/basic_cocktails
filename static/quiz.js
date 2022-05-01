3a33a7a43e61df6d3e845d4a38a5ee54170560d0
Matthew Kim <HeajasAir@dyn-160-39-143-66.dyn.columbia.edu>
2022-05-01 18:12:01 -0400

    made some more styling changes

--- static/quiz.js
+++ static/quiz.js
@@ -6,26 +6,29 @@ function create_link(cocktail) {
     
     let curCocktail = cocktail
     let title = curCocktail["title"]
-    let image = curCocktail["image"]
     let altText = curCocktail["alt_text"]
     let col = $('<div class="col-md-4"></div>')
 
-    //adjust width NEW 
-    let cardContainer = $('<div class="text-center"> </div>')
+    //adjust width NEW
+    let cardContainer = $('<div class="card text-center"> </div>')
     let cardBody = $('<div class="card-body"> </div>')
-    let cardText = $('<p class="card-text"> </p>')
-    $(cardText).text(title)
+    let cardTitle = $('<div class="quiz_card_title card-title">' + title + "</div>")
+    let cardText = $('<p class="card-text">Test your knowledge on how to make a ' + title + ' cocktail</p>')
+    $(cardBody).append(cardTitle)
     $(cardBody).append(cardText)
 
     quiz_id = (curCocktail['id'] * 3) - 2
     // console.log(quiz_id)
     
-    let content = $("<a href='/quiz/" + quiz_id + "'><img class = 'card-img-top' src='"+image+"'"+" alt='"+altText+"'" + " width= '100%'></a");
-    // let content = $("<img class = 'card-img-top' src='"+image+"'"+" alt='"+altText+"'" + " width= '100%'>");
+    let button = $('<button class="btn take_quiz_button">Take Quiz</button>' )
+    
+    $(button).click(function(){
+        document.location.href = "/quiz/" + quiz_id
+    })
     
     //NEW 
     $(cardContainer).append(cardBody)
-    $(cardContainer).append(content)
+    $(cardContainer).append(button)
     $(col).append(cardContainer)
     $("#cocktail-list-div").append(col) 
 
